bb64d5397d7cb383c863e6a0e492cce828387a30
Jean Guinami <37559113+jeanguinami@users.noreply.github.com>
2020-03-20 19:59:06 -0300

    First commit

---
+++ characters/src/test/java/io/jeanfrias/characters/CharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jeanfrias.characters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
